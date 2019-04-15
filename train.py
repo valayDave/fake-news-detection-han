8fe0384f8a936aa3ee77f052f6bd5e237c7fd8bd
Valay Dave <valaydave@valays-mbp.3520.dhcp.asu.edu>
2019-04-14 17:52:57 -0700

    Modified the headline tokenization in the 3han.

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ learning_rate = 0.6
 REG_PARAM = 1e-13
 PLOT_FOLDER = os.path.join(my_path, 'plots/')
 MODEL_FOLDER = os.path.join(my_path, 'models/')
-sample_dataset = False
+sample_dataset = True
 NUM_SAMPLES = 20
 NUM_EPOCHS = 25
 DROPOUT_VALUE = 0.5
@@ -216,11 +216,13 @@ def generate_han_embedding_matrix(data_frame,title_bool):
     tokenizer = Tokenizer(num_words=max_features, oov_token=True)
     tokenizer.fit_on_texts(texts)
 
-    headline_tokenizer = Tokenizer(num_words=max_features, oov_token=True)
-    headline_tokenizer.fit_on_texts(headlines)
+    # headline_tokenizer = Tokenizer(num_words=max_features, oov_token=True)
+    # headline_tokenizer.fit_on_texts(headlines)
 
     tokenized_body = np.zeros((len(texts), max_senten_num, max_senten_len), dtype='int32')
     for i, sentences in enumerate(paras):
+        #print(sentences)
+        #print(i)
         for j, sent in enumerate(sentences):
             if j< max_senten_num:
                 wordTokens = text_to_word_sequence(sent)
@@ -228,16 +230,23 @@ def generate_han_embedding_matrix(data_frame,title_bool):
                 for _, word in enumerate(wordTokens):
                     try:
                         if k<max_senten_len and tokenizer.word_index[word]<max_features:
+                            # print(word,tokenizer.word_index[word])   
                             tokenized_body[i,j,k] = tokenizer.word_index[word]
                             k=k+1
                     except:
                         print(word)
                         pass
-    
-    #TODO: Figure the above
-    headline_sequence = headline_tokenizer.texts_to_sequences(headlines)
-    tokenized_headlines = pad_sequences(headline_sequence,maxlen=max_senten_len)
-    #Converts Labels to Digits. 
+    #Datastructure from the above [i:Article_Number,j:Sentence_number,k:Word Index]
+    tokenized_headlines = np.zeros((len(texts), max_senten_len), dtype='int32')
+    for i,headline in enumerate(headlines):
+        wordTokens = text_to_word_sequence(headline)
+        for j,word in enumerate(wordTokens):
+            try:
+                if j<max_senten_len and tokenizer.word_index[word]<max_features:
+                    tokenized_headlines[i,j] = tokenizer.word_index[word]
+            except:
+                print(word)
+                pass
     seperated_labels = pd.get_dummies(labels)
     indices = np.arange(tokenized_body.shape[0])
     np.random.shuffle(indices)
